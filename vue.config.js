a3f9d7b355db582a6064f3d29a0e8e3d662a4b91
MeowTzyッ <149583086+yuichi192168@users.noreply.github.com>
2026-02-02 12:11:36 +0800

    Add files via upload
    update config

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// vue.config.js
+module.exports = {
+  configureWebpack: {
+    entry: './src/main.ts'
+  }
+}
